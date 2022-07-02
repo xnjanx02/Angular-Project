d15c29b395e14f7d87bd514a14081bc49c8dd2e1
xnjanx02 <anjana.jk009@gmail.com>
2022-07-02 14:11:41 +0530

    Faculty component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
+import { FacultyComponent } from './faculty/faculty.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent
+    StudentComponent,
+    FacultyComponent
   ],
   imports: [
     BrowserModule,
